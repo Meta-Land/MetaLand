8eada46ed6eca5b27c3b8dc7912b73220e38b5ee
StrayRaider <41emrekaya919@gmail.com>
2023-05-15 19:33:57 +0300

    sign up windows added

---
+++ main/windows/signUpW.h
@@ -0,0 +1,29 @@
+#include <QMainWindow>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QMessageBox>
+
+class SignUpScreen : public QMainWindow
+
+{
+    private:
+        //id ve password girişleri için olan widgetlerin ön tanımlaması
+        QLineEdit *IdEntery;
+        QLineEdit *PasswordEntery;
+        QLineEdit *PasswordControlEntery;
+        QPushButton *signInButton;
+        QPushButton *signUpButton;
+        QPushButton *backButton;
+        QLabel *picLabel;
+
+        Q_OBJECT
+
+    // Fonksiyon ön tanımlamaları slots bölümünde yapılıyor.
+    private slots:
+        void signUpClicked();
+
+    public:
+        SignUpScreen(QMainWindow *parent = nullptr);
+        ~SignUpScreen();
+};
